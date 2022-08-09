28a47ed3aa9d79988181b0c241ca07db68773a74
NoLifeRTTS <alex.shevelev.01@mail.ru>
2022-08-09 12:48:59 +0700

    второй блок + недоделанная форма

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
+
     $('.header__burger').on('click', function() {
         $(this).toggleClass('active');
         $('.header__menu').toggleClass('active');
+    });
+
+    $('.order__percent').text($(".order__range").val() + " %");
+    $(".order__range").on("input change",function(){
+        $(".order__percent").html($(this).val() + " %");
     });
 });
