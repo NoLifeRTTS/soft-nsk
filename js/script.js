197ff0252e30f512f297d2aebe30e1ae9ebe28e4
NoLifeRTTS <alex.shevelev.01@mail.ru>
2022-08-10 18:35:36 +0700

    готовый проект
    футер + оптимизация веса

--- js/script.js
+++ js/script.js
@@ -9,4 +9,12 @@ $(document).ready(function() {
     $(".order__range").on("input change",function(){
         $(".order__percent").html($(this).val() + " %");
     });
+
+    $(".order__scroll").on('click',function(e){
+        e.preventDefault();
+        let top = $($(this).attr("data-scroll")).offset().top - 65;
+        $(".header__burger").removeClass("active");
+        $(".header__menu").removeClass('active');
+        $("html, body").animate({scrollTop: top}, 500);
+    });
 });
